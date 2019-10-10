0bdbec38823d9b4815d5d79ca094b8c410d5dcca
MegaFooby <cameron.davies2@ucalgary.ca>
2019-10-10 15:09:14 -0600

    Prints name, interfaces, and methods

--- Inspector.java
+++ Inspector.java
@@ -1,4 +1,6 @@
 
+import java.lang.reflect.*;
+
 public class Inspector {
 
     public void inspect(Object obj, boolean recursive) {
@@ -7,6 +9,35 @@ public class Inspector {
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+		//Declaring class
+		System.out.print("1. " + c.getName() + "\n");
+		
+		//interfaces
+		for(Class enterface : c.getInterfaces()) {
+			System.out.print(enterface + "\n");
+		}
+		
+		//methods
+		for(Method method : c.getMethods()) {
+			String tmp = method.getName() + "(";
+			boolean put_comma = false;
+			for(Class parameter : method.getParameterTypes()) {
+				if(put_comma) {
+					tmp += ", ";
+				}
+				tmp += parameter.getName();
+				put_comma = true;
+			}
+			System.out.print(tmp + ")\n");
+		}
     }
+
+	public static void main(String args[]) {
+		try {
+			new Inspector().inspect(Class.forName(args[0]).newInstance(), true);
+		} catch(Exception e) {
+			System.out.print("Class not found\n");
+		}
+	}
 
 }
