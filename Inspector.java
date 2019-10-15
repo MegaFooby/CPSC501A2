54e919aec9d1f028d2b5bfbba6a2e82f94327b57
MegaFooby <cameron.davies2@ucalgary.ca>
2019-10-15 11:42:11 -0600

    Split inspectClass into multiple smaller methods

--- Inspector.java
+++ Inspector.java
@@ -3,15 +3,54 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class Inspector {
-
+	
+	private ArrayList<Object> explore = new ArrayList<Object>();
+    
     public void inspect(Object obj, boolean recursive) {
         Class c = obj.getClass();
         inspectClass(c, obj, recursive, 0);
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-		ArrayList<Object> explore = new ArrayList<Object>();
 		
+		this.print_title(c);
+		
+		if(c.getDeclaredFields().length != 0) {
+			this.print_fields(c, obj);
+			System.out.print("\n");
+		}
+		
+		if(c.getDeclaredConstructors().length != 0) {
+			this.print_constructors(c);
+			System.out.print("\n");
+		}
+		
+		if(c.getDeclaredMethods().length != 0) {
+			this.print_methods(c);
+			System.out.print("\n");
+		}
+		
+		System.out.print("\n");
+		
+		//recursivly print superclasses
+		if((recursive || depth == 0) && c.getSuperclass() != null) {
+			this.inspectClass(c.getSuperclass(), obj, recursive, depth+1);
+		}
+		
+		//recursivly checks out interfaces
+		if((recursive || depth == 0)) {
+			for(Class enterface : c.getInterfaces()) {
+				this.inspectClass(enterface, obj, true, depth+1);
+			}
+		}
+    }
+    
+    /**
+     * Prints the name, modifiers, interfaces, and superclass of the specified class
+     * 
+     * @param c	The class to inspect
+     */
+    public void print_title(Class c) {
 		//Declaring class
 		System.out.print(Modifier.toString(c.getModifiers()) + " " + c.getName());
 		
@@ -31,7 +70,9 @@ public class Inspector {
 			System.out.print(" extends " + c.getSuperclass().getName());
 		}
 		System.out.print("\n");
-		
+	}
+	
+	public void print_fields(Class c, Object obj) {
 		//fields
 		for(Field f : c.getDeclaredFields()) {
 			try {
@@ -42,14 +83,13 @@ public class Inspector {
 			}
 			System.out.print("\n");
 		}
-		if(c.getDeclaredFields().length != 0) {
-			System.out.print("\n");
-		}
-		
+	}
+	
+	public void print_constructors(Class c) {
 		//constructors
 		for(Constructor t : c.getDeclaredConstructors()) {
 			System.out.print("\t" + Modifier.toString(t.getModifiers()) + " " + t.getName() + "(");
-			loop_start = true;
+			boolean loop_start = true;
 			for(Class parameter : t.getParameterTypes()) {
 				if(!loop_start) {
 					System.out.print(", ");
@@ -59,53 +99,39 @@ public class Inspector {
 			}
 			System.out.print(")\n");
 		}
-		if(c.getDeclaredConstructors().length != 0) {
-			System.out.print("\n");
-		}
-		
+	}
+	
+	public void print_methods(Class c) {
 		//methods
 		for(Method method : c.getDeclaredMethods()) {
 			//name, modifiers, return type
-			String tmp = "\t" + Modifier.toString(method.getModifiers()) + " " + method.getReturnType().getName() + " " + method.getName() + "(";
+			System.out.print("\t" + Modifier.toString(method.getModifiers()) + " " + method.getReturnType().getName() + " " + method.getName() + "(");
 			
 			//parameters
-			loop_start = true;
+			boolean loop_start = true;
 			for(Class parameter : method.getParameterTypes()) {
 				if(!loop_start) {
-					tmp += ", ";
+					System.out.print(", ");
 				}
-				tmp += parameter.getName();
+				System.out.print(parameter.getName());
 				loop_start = false;
 			}
-			tmp += ")";
+			System.out.print(")");
 			
 			//exceptions
 			loop_start = true;
 			for(Class eggception : method.getExceptionTypes()) {
 				if(!loop_start) {
-					tmp += ", ";
+					System.out.print(", ");
 				} else {
-					tmp += " throws ";
+					System.out.print(" throws ");
 				}
-				tmp += eggception.getName();
+				System.out.print(eggception.getName());
 				loop_start = false;
 			}
-			System.out.print(tmp + "\n");
+			System.out.print("\n");
 		}
-		System.out.print("\n");
-		
-		//recursivly print superclasses
-		if((recursive || depth == 0) && c.getSuperclass() != null) {
-			this.inspectClass(c.getSuperclass(), obj, recursive, depth+1);
-		}
-		
-		//recursivly checks out interfaces
-		if((recursive || depth == 0)) {
-			for(Class enterface : c.getInterfaces()) {
-				this.inspectClass(enterface, obj, true, depth+1);
-			}
-		}
-    }
+	}
 
 	/**
 	 * A test method which recursively prints out a class
